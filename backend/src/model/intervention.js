9748beda83aaa3d6b940b1d8f9b2d9a1ffb3928b
zi <161949268+strangeshands@users.noreply.github.com>
2025-06-07 23:50:36 +0800

    Populate sponsored member & intervention; fixed few properties on schema

--- backend/src/model/intervention.js
+++ backend/src/model/intervention.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/*
+     UPDATE: Removed sponsor_id as interventions can already be referenced from sponsored_member object.
+ */
 const InterventionsSchema = new Schema ({
      intervention_type: {
           type: mongoose.Schema.Types.ObjectId, 
           ref: 'Intervention Type',
 
           required: true
-     },
-     sponsor_id: {
-          type: mongoose.Schema.Types.ObjectId, 
-          ref: 'Sponsored Member',
-
-          required: true,
      },
      objective: {
           type: String, 
