34484de6778aec1c5270db6594b22751d9906610
Kelsey <gabriellemkelsey@gmail.com>
2025-07-01 17:49:34 +0800

    Create initial form for Home Visitation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import CaseFrontend from "./pages/case-report-page/case-frontend.jsx";
 import FinancialAssessmentForm from "./pages/intervention-forms/financial-assessment.jsx";
 import CounsellingForm from "./pages/intervention-forms/counselling.jsx";
 import CorrespondenceForm from "./pages/intervention-forms/correspondence.jsx";
+import HomeVisitationForm from "./pages/intervention-forms/home-visitation.jsx";
 import "./index.css";
 
 //we need to add routes pa here for going to other pages so the actual routes are here we add the module Case to load that page
@@ -28,6 +29,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                 <Route
                     path="/correspondence-form"
                     element={<CorrespondenceForm />}
+                />
+                <Route
+                    path="/home-visitation-form"
+                    element={<HomeVisitationForm />}
                 />
             </Routes>
         </BrowserRouter>
---
+++ frontend/src/pages/intervention-forms/home-visitation.jsx
@@ -0,0 +1,133 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Signature from "../../Components/Signature";
+import { TextInput, TextArea } from "../../Components/TextField";
+
+function HomeVisitationForm() {
+    const [data, setData] = useState({
+        form_num: "3",
+        first_name: "Hepzhi-Bah",
+        middle_name: "Gamac",
+        last_name: "Tolentino",
+        grade_year_course: "",
+        years_in_program: "",
+        date: "",
+        community: "",
+        sponsor_name: "",
+        family_type: "",
+        father_first_name: "",
+        father_middle_name: "",
+        father_last_name: "",
+        father_work: "",
+        father_income: "",
+        mother_first_name: "",
+        mother_middle_name: "",
+        mother_last_name: "",
+        mother_work: "",
+        mother_income: "",
+        sm_progress: "",
+        family_progress: "",
+        recommendation: "",
+        agreement: "",
+    });
+
+    const [familyMembers, setFamilyMembers] = useState([
+        {
+            name: "Ana Victoria Angat",
+            age: 20,
+            income: 100000.0,
+            civilStatus: "Single",
+            occupation: "Software Developer",
+            education: "Undergraduate",
+            relationship: "Sister",
+        },
+        {
+            name: "Marvin Ivan Mangubat",
+            age: 21,
+            income: 0.0,
+            civilStatus: "Divorced",
+            occupation: "Unemployed",
+            education: "Undergraduate",
+            relationship: "Sister",
+        },
+        {
+            name: "Jose Miguel Espinosa",
+            age: 21,
+            income: 100000.0,
+            civilStatus: "Single",
+            occupation: "Producer",
+            education: "Undergraduate",
+            relationship: "Brother",
+        },
+    ]);
+
+    const [observation_findings, setObservationFindings] = useState([
+        "Observation 1",
+        "Observation 2",
+        "Observation 3",
+    ]);
+
+    const [interventions, setInterventions] = useState([
+        "Intervention 1",
+        "Intervention 2",
+        "Intervention 3",
+    ]);
+
+    const [last_name, setLastName] = useState(data?.last_name || "");
+    const [middle_name, setMiddleName] = useState(data?.middle_name || "");
+    const [first_name, setFirstName] = useState(data?.first_name || "");
+    const [form_num, setFormNum] = useState(data?.form_num || "");
+    const [grade_year_course, setGradeYearCourse] = useState(
+        data?.grade_year_course || "",
+    );
+    const [years_in_program, setYearsInProgram] = useState(
+        data?.years_in_program || "",
+    );
+    const [date, setDate] = useState(data?.date || "");
+    const [xommunity, setCommunity] = useState(data?.community || "");
+    const [sponsor_name, setSponsorName] = useState(data?.sponsor_name || "");
+    const [family_type, setFamilyType] = useState(data?.family_type || "");
+    const [father_first_name, setFatherFirstName] = useState(
+        data?.father_first_name || "",
+    );
+    const [father_middle_name, setFatherMiddleName] = useState(
+        data?.father_middle_name || "",
+    );
+    const [father_last_name, setFatherLastName] = useState(
+        data?.father_last_name || "",
+    );
+    const [father_work, setFatherWork] = useState(data?.father_work || "");
+    const [father_income, setFatherIncome] = useState(
+        data?.father_income || "",
+    );
+    const [mother_first_name, setMotherFirstName] = useState(
+        data?.mother_first_name || "",
+    );
+    const [mother_middle_name, setMotherMiddleName] = useState(
+        data?.mother_middle_name || "",
+    );
+    const [mother_last_name, setMotherLastName] = useState(
+        data?.mother_last_name || "",
+    );
+    const [mother_work, setMotherWork] = useState(data?.mother_work || "");
+    const [mother_income, setMotherIncome] = useState(
+        data?.mother_income || "",
+    );
+    const [sm_progress, setSMProgress] = useState(data?.sm_progress || "");
+    const [family_progress, setFamilyProgress] = useState(
+        data?.family_progress || "",
+    );
+    const [recommendation, setRecommendation] = useState(
+        data?.recommendation || "",
+    );
+    const [agreement, setAgreement] = useState(data?.agreement || "");
+
+    return (
+        <main className="flex max-w-7xl flex-col items-center justify-center gap-10 px-10">
+            <h4 className="header-sm self-end">Form #: {form_num}</h4>
+            <h3 className="header-md">Home Visitation Report</h3>
+        </main>
+    );
+}
+
+export default HomeVisitationForm;
