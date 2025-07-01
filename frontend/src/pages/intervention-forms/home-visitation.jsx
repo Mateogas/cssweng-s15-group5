6a370960203f2e39690120f5a8667ef216810ec3
Kelsey <gabriellemkelsey@gmail.com>
2025-07-01 18:18:24 +0800

    Add General Information to Home Visitation Form

--- frontend/src/pages/intervention-forms/home-visitation.jsx
+++ frontend/src/pages/intervention-forms/home-visitation.jsx
@@ -84,7 +84,7 @@ function HomeVisitationForm() {
         data?.years_in_program || "",
     );
     const [date, setDate] = useState(data?.date || "");
-    const [xommunity, setCommunity] = useState(data?.community || "");
+    const [community, setCommunity] = useState(data?.community || "");
     const [sponsor_name, setSponsorName] = useState(data?.sponsor_name || "");
     const [family_type, setFamilyType] = useState(data?.family_type || "");
     const [father_first_name, setFatherFirstName] = useState(
@@ -160,6 +160,47 @@ function HomeVisitationForm() {
                                 label="Year/s in the Program"
                                 value={years_in_program}
                                 setValue={setYearsInProgram}
+                            ></TextInput>
+                            <div className="flex items-center gap-10">
+                                <p className="label-base w-44">Family Type</p>
+                                <select
+                                    name="family_type"
+                                    id="family_type"
+                                    value={family_type}
+                                    onChange={(e) => setFamilyType(e.target.value)}
+                                    className="label-base text-input"
+                                >
+                                    <option value="" className="body-base">Select</option>
+                                    <option value="nuclear" className="body-base">Nuclear</option>
+                                    <option value="extended" className="body-base">Extended</option>
+                                    <option value="blended" className="body-base">Blended</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex w-full flex-col gap-5 rounded-[0.5rem] border border-[var(--border-color)] p-5">
+                    <div className="flex border-b border-[var(--border-color)]">
+                        <h4 className="header-sm">General Information</h4>
+                    </div>
+                    <div className="inline-flex items-center justify-center gap-10">
+                        <div className="flex flex-col gap-5">
+                            <TextInput
+                                label="Date"
+                                value={date}
+                                setValue={setDate}
+                            ></TextInput>
+                            <TextInput
+                                label="Community"
+                                value={community}
+                                setValue={setCommunity}
+                            ></TextInput>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <TextInput
+                                label="Sponsor Name"
+                                value={sponsor_name}
+                                setValue={setSponsorName}
                             ></TextInput>
                         </div>
                     </div>
