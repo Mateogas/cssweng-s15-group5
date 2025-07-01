8e6d64e798e4fda084219e85fa3defe0e381eb45
Kelsey <gabriellemkelsey@gmail.com>
2025-07-01 21:27:56 +0800

    Add Client Info to Case Closure

--- frontend/src/pages/case-closure.jsx
+++ frontend/src/pages/case-closure.jsx
@@ -36,7 +36,58 @@ function CaseClosure() {
         <main className="flex max-w-7xl flex-col items-center justify-center gap-10 px-10">
             <h4 className="header-sm self-end">Form #: {form_num}</h4>
             <h3 className="header-md">Case Closure Report</h3>
-
+            
+            <section className="flex w-full flex-col gap-10">
+                <div className="flex w-full flex-col gap-5 rounded-[0.5rem] border border-[var(--border-color)] p-5">
+                    <div className="flex border-b border-[var(--border-color)]">
+                        <h4 className="header-sm">Sponsored Member</h4>
+                    </div>
+                    <div className="inline-flex items-center justify-center gap-10">
+                        <div className="flex flex-col gap-5">
+                            <TextInput
+                                label="Last Name"
+                                value={last_name}
+                                setValue={setLastName}
+                            ></TextInput>
+                            <TextInput
+                                label="First Name"
+                                value={first_name}
+                                setValue={setFirstName}
+                            ></TextInput>
+                            <TextInput
+                                label="Middle Name"
+                                value={middle_name}
+                                setValue={setMiddleName}
+                            ></TextInput>
+                            <TextInput
+                                label="CH ID #"
+                                value={ch_number}
+                                setValue={setCHNumber}
+                            ></TextInput>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <TextInput
+                                label="Date of Birth"
+                                value={dob}
+                                setValue={setDOB}
+                            ></TextInput>
+                            <TextInput
+                                label="Religion"
+                                value={religion}
+                                setValue={setReligion}
+                            ></TextInput>
+                            <div className="flex gap-10">
+                                <p className="label-base w-44">Address</p>
+                                <textarea
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
+                                    className="text-area"
+                                ></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
