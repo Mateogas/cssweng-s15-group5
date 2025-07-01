d49f565a0133f609eb47c80a4a0eb348b188ebb6
Kelsey <gabriellemkelsey@gmail.com>
2025-07-01 17:03:57 +0800

    Add Sponsored Member Info to Correspondence Form

--- frontend/src/pages/intervention-forms/correspondence.jsx
+++ frontend/src/pages/intervention-forms/correspondence.jsx
@@ -28,12 +28,68 @@ function CorrespondenceForm() {
     const [address, setAddress] = useState(data?.address || "");
     const [sponsor_name, setSponsorName] = useState(data?.sponsor_name || "");
     const [subproject, setSubproject] = useState(data?.subproject || "");
-    const [date_of_sponsorship, setDateOfSponsorship] = useState(data?.date_of_sponsorship || "");
-    
+    const [date_of_sponsorship, setDateOfSponsorship] = useState(
+        data?.date_of_sponsorship || "",
+    );
+
     return (
         <main className="flex max-w-7xl flex-col items-center justify-center gap-10 px-10">
             <h4 className="header-sm self-end">Form #: {form_num}</h4>
-            <h3 className="header-md">SMs, Families, and SHGs Intervention Plan</h3>
+            <h3 className="header-md">
+                SMs, Families, and SHGs Intervention Plan
+            </h3>
+
+            <section className="flex w-full flex-col gap-10">
+                <div className="flex w-full flex-col gap-5 rounded-[0.5rem] border border-[var(--border-color)] p-5">
+                    <div className="flex border-b border-[var(--border-color)]">
+                        <h4 className="header-sm">Sponsored Member</h4>
+                    </div>
+                    <div className="inline-flex items-center justify-center gap-10">
+                        <div className="flex flex-col gap-5">
+                            <TextInput
+                                label="Last Name"
+                                value={last_name}
+                                setValue={setLastName}
+                            ></TextInput>
+                            <TextInput
+                                label="First Name"
+                                value={first_name}
+                                setValue={setFirstName}
+                            ></TextInput>
+                            <TextInput
+                                label="Middle Name"
+                                value={middle_name}
+                                setValue={setMiddleName}
+                            ></TextInput>
+                            <TextInput
+                                label="CH ID #"
+                                value={ch_number}
+                                setValue={setCHNumber}
+                            ></TextInput>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <TextInput
+                                label="Date of Birth"
+                                value={dob}
+                                setValue={setDOB}
+                            ></TextInput>
+                            <TextInput
+                                label="School"
+                                value={school}
+                                setValue={setSchool}
+                            ></TextInput>
+                            <div className="flex gap-10">
+                                <p className="label-base w-44">Address</p>
+                                <textarea
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
+                                    className="text-area"
+                                ></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
