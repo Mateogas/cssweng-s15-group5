6d6a710dc2b8a11865c35fecf515c3bd4253d9ec
Kelsey <gabriellemkelsey@gmail.com>
2025-07-04 16:12:37 +0800

    Layout fix for Progress Report

--- frontend/src/pages/progress-report.jsx
+++ frontend/src/pages/progress-report.jsx
@@ -102,175 +102,170 @@ function ProgressReport() {
     /********** FUNCTIONS **********/
 
     return (
-        <main className="flex w-7xl flex-col items-center justify-center gap-10 rounded-lg border border-[var(--border-color)] p-10">
-            <h4 className="header-sm self-end">Form #: {form_num}</h4>
-            <h3 className="header-md">Individual Progress Report</h3>
-
-            {/* Sponsored Member and General Info */}
-            <section className="flex w-full flex-col gap-10">
-                <div className="flex w-full flex-col gap-5 rounded-[0.5rem] border border-[var(--border-color)] p-5">
-                    <div className="flex border-b border-[var(--border-color)]">
-                        <h4 className="header-sm">Sponsored Member</h4>
-                    </div>
-                    <div className="inline-flex items-center justify-center gap-10">
-                        <div className="flex flex-col gap-5">
-                            <TextInput
-                                label="Last Name"
-                                value={last_name}
-                                disabled={true}
-                            ></TextInput>
-                            <TextInput
-                                label="First Name"
-                                value={first_name}
-                                disabled={true}
-                            ></TextInput>
-                            <TextInput
-                                label="Middle Name"
-                                value={middle_name}
-                                disabled={true}
-                            ></TextInput>
+        <main className="flex justify-center">
+            <div className="flex w-7xl flex-col items-center justify-center gap-10 rounded-lg border border-[var(--border-color)] p-10">
+                <h4 className="header-sm self-end">Form #: {form_num}</h4>
+                <h3 className="header-md">Individual Progress Report</h3>
+                {/* Sponsored Member and General Info */}
+                <section className="flex w-full flex-col gap-10">
+                    <div className="flex w-full flex-col gap-5 rounded-[0.5rem] border border-[var(--border-color)] p-5">
+                        <div className="flex border-b border-[var(--border-color)]">
+                            <h4 className="header-sm">Sponsored Member</h4>
                         </div>
-                        <div className="flex flex-col gap-5">
-                            <TextInput
-                                label="CH ID #"
-                                value={ch_number}
-                                disabled={true}
-                            ></TextInput>
-                            <DateInput
-                                label="Date of Birth"
-                                value={dob}
-                                disabled={true}
-                            ></DateInput>
-                            <TextInput
-                                label="Age"
-                                value={age}
-                                disabled={true}
-                            ></TextInput>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex w-full flex-col gap-5 rounded-[0.5rem] border border-[var(--border-color)] p-5">
-                    <div className="flex border-b border-[var(--border-color)]">
-                        <h4 className="header-sm">General Information</h4>
-                    </div>
-                    <div className="inline-flex items-center justify-center gap-10">
-                        <div className="flex flex-col gap-5">
-                            <TextInput
-                                label="Sub-Project"
-                                value={subproject}
-                                disabled={true}
-                            ></TextInput>
-                            <DateInput
-                                label="Date Accomplished"
-                                value={date_accomplished}
-                                setValue={setDateAccomplished}
-                            ></DateInput>
-                            <TextInput
-                                label="Period Covered"
-                                value={period_covered}
-                                setValue={setPeriodCovered}
-                            ></TextInput>
-                        </div>
-                        <div className="flex flex-col gap-5">
-                            <TextInput
-                                label="Name of Sponsor"
-                                value={sponsor_name}
-                                setValue={setSponsorName}
-                            ></TextInput>
-                            <DateInput
-                                label="Sponsorship Begin Date"
-                                value={sponsorship_date}
-                                setValue={setSponsorshipDate}
-                            ></DateInput>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            {/* Update/Developmert */}
-            <section className="flex w-full flex-col gap-10">
-                <div className="flex w-full flex-col gap-5">
-                    <h3 className="header-md">Update/Development</h3>
-                    <h4 className="header-sm">
-                        e.g. Education, Health, Socio-Economic, Behavioral,
-                        Social, etc.
-                    </h4>
-                </div>
-                <div className="flex w-full gap-10">
-                    <TextArea
-                        label="Sponsored Member (observation)"
-                        value={sm_update}
-                        setValue={setSMUpdate}
-                    ></TextArea>
-                    <TextArea
-                        label="Family"
-                        value={family_update}
-                        setValue={setFamilyUpdate}
-                    ></TextArea>
-                </div>
-            </section>
-
-            {/* Services to Family */}
-            <section className="flex w-full">
-                <TextArea
-                    label="Services Rendered to the Family"
-                    value={services_to_family}
-                    setValue={setServicesToFamily}
-                ></TextArea>
-            </section>
-
-            {/* Participation */}
-            <section className="flex w-full">
-                <TextArea
-                    label="Participation in the Community"
-                    sublabel="Include care for the environment"
-                    value={participation}
-                    setValue={setParticipation}
-                ></TextArea>
-            </section>
-
-            {/* Relationship to Sponsor and Unbound */}
-            <section className="flex w-full flex-col gap-5">
-                <h4 className="header-sm">Relationship to Sponsor & Unbound</h4>
-                <div className="flex gap-24">
-                    {relation_to_sponsor.map((q) => (
-                        <div
-                            key={q.id}
-                            className="flex flex-col justify-end gap-5"
-                        >
-                            <p className="body-base">{q.text}</p>
-                            <div className="flex gap-8">
-                                {options.map((option) => (
-                                    <label
-                                        key={option}
-                                        className="flex items-center gap-2.5"
-                                    >
-                                        <input
-                                            type="checkbox"
-                                            name={q.id}
-                                            value={option}
-                                            checked={responses[q.id] === option}
-                                            onChange={() =>
-                                                handleCheckboxChange(
-                                                    q.id,
-                                                    option,
-                                                )
-                                            }
-                                        />
-                                        {option}
-                                    </label>
-                                ))}
+                        <div className="inline-flex items-center justify-center gap-10">
+                            <div className="flex flex-col gap-5">
+                                <TextInput
+                                    label="Last Name"
+                                    value={last_name}
+                                    disabled={true}
+                                ></TextInput>
+                                <TextInput
+                                    label="First Name"
+                                    value={first_name}
+                                    disabled={true}
+                                ></TextInput>
+                                <TextInput
+                                    label="Middle Name"
+                                    value={middle_name}
+                                    disabled={true}
+                                ></TextInput>
+                            </div>
+                            <div className="flex flex-col gap-5">
+                                <TextInput
+                                    label="CH ID #"
+                                    value={ch_number}
+                                    disabled={true}
+                                ></TextInput>
+                                <DateInput
+                                    label="Date of Birth"
+                                    value={dob}
+                                    disabled={true}
+                                ></DateInput>
+                                <TextInput
+                                    label="Age"
+                                    value={age}
+                                    disabled={true}
+                                ></TextInput>
                             </div>
                         </div>
-                    ))}
+                    </div>
+                    <div className="flex w-full flex-col gap-5 rounded-[0.5rem] border border-[var(--border-color)] p-5">
+                        <div className="flex border-b border-[var(--border-color)]">
+                            <h4 className="header-sm">General Information</h4>
+                        </div>
+                        <div className="inline-flex items-center justify-center gap-10">
+                            <div className="flex flex-col gap-5">
+                                <TextInput
+                                    label="Sub-Project"
+                                    value={subproject}
+                                    disabled={true}
+                                ></TextInput>
+                                <DateInput
+                                    label="Date Accomplished"
+                                    value={date_accomplished}
+                                    setValue={setDateAccomplished}
+                                ></DateInput>
+                                <TextInput
+                                    label="Period Covered"
+                                    value={period_covered}
+                                    setValue={setPeriodCovered}
+                                ></TextInput>
+                            </div>
+                            <div className="flex flex-col gap-5">
+                                <TextInput
+                                    label="Name of Sponsor"
+                                    value={sponsor_name}
+                                    setValue={setSponsorName}
+                                ></TextInput>
+                                <DateInput
+                                    label="Sponsorship Begin Date"
+                                    value={sponsorship_date}
+                                    setValue={setSponsorshipDate}
+                                ></DateInput>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {/* Update/Developmert */}
+                <section className="flex w-full flex-col gap-10">
+                    <div className="flex w-full flex-col gap-5">
+                        <h3 className="header-md">Update/Development</h3>
+                        <h4 className="header-sm">
+                            e.g. Education, Health, Socio-Economic, Behavioral,
+                            Social, etc.
+                        </h4>
+                    </div>
+                    <div className="flex w-full gap-10">
+                        <TextArea
+                            label="Sponsored Member (observation)"
+                            value={sm_update}
+                            setValue={setSMUpdate}
+                        ></TextArea>
+                        <TextArea
+                            label="Family"
+                            value={family_update}
+                            setValue={setFamilyUpdate}
+                        ></TextArea>
+                    </div>
+                </section>
+                {/* Services to Family */}
+                <section className="flex w-full">
+                    <TextArea
+                        label="Services Rendered to the Family"
+                        value={services_to_family}
+                        setValue={setServicesToFamily}
+                    ></TextArea>
+                </section>
+                {/* Participation */}
+                <section className="flex w-full">
+                    <TextArea
+                        label="Participation in the Community"
+                        sublabel="Include care for the environment"
+                        value={participation}
+                        setValue={setParticipation}
+                    ></TextArea>
+                </section>
+                {/* Relationship to Sponsor and Unbound */}
+                <section className="flex w-full flex-col gap-5">
+                    <h4 className="header-sm">Relationship to Sponsor & Unbound</h4>
+                    <div className="flex gap-24">
+                        {relation_to_sponsor.map((q) => (
+                            <div
+                                key={q.id}
+                                className="flex flex-col justify-end gap-5"
+                            >
+                                <p className="body-base">{q.text}</p>
+                                <div className="flex gap-8">
+                                    {options.map((option) => (
+                                        <label
+                                            key={option}
+                                            className="flex items-center gap-2.5"
+                                        >
+                                            <input
+                                                type="checkbox"
+                                                name={q.id}
+                                                value={option}
+                                                checked={responses[q.id] === option}
+                                                onChange={() =>
+                                                    handleCheckboxChange(
+                                                        q.id,
+                                                        option,
+                                                    )
+                                                }
+                                            />
+                                            {option}
+                                        </label>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+                {/* Buttons */}
+                <div className="mt-10 flex w-[22.5rem] justify-between">
+                    <button className="btn-outline-rounded">Cancel</button>
+                    <button className="btn-primary">Create Progress Report</button>
                 </div>
-            </section>
-
-            {/* Buttons */}
-            <div className="mt-10 flex w-[22.5rem] justify-between">
-                <button className="btn-outline-rounded">Cancel</button>
-                <button className="btn-primary">Create Progress Report</button>
             </div>
         </main>
     );
