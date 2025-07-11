e87c819ec9f366f759f484bde84cf62cd9a72382
zi <161949268+strangeshands@users.noreply.github.com>
2025-07-12 01:07:21 +0800

    Started 2.2.1 (backend); cleaned up code on employee model

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -3,6 +3,9 @@
  *        > handles login, logout, and signup
  */
 
+const mongoose = require('mongoose');
+const Employee = require('../model/employee');
+
 // ================================================== //
 
 /**
@@ -13,12 +16,47 @@ const renderLoginPage = async (req, res) => {
 }
 
 /**
- *   Handle log in feature
- *        > username and password must match
- *        > redirect to home page
+ *   Handles logging in, username and password must match
+ *   @returns  active_user : an Employee object (if success)
+ *             errorMsg if fail
  */
 const loginUser = async (req, res) => {
      // code here
+     try {
+          const { email, password } = req.body
+          const remember_me = req.body.rememberMe;
+
+          var authFlag = false
+
+          const active_user = await Employee.findOne({ email: email });
+
+          if (!active_user)
+               return res.send(200).json({ errorMsg: "Invalid email or password" })
+
+          // assumed account password is already hashed
+          const isPasswordValid = await active_user.comparePassword(password)
+          if (!isPasswordValid) {
+               // force, compare text
+               if (password === active_user.password) {
+                    // [TO UPDATE] :: uncomment once session is set up
+                    // req.session.user = active_user;
+                    return res.send(200).json( active_user )
+               }
+               // case that none really matched
+               else
+                    return res.send(200).json({ errorMsg: "Invalid email or password" })
+          }
+
+          // [TO UPDATE] :: uncomment once session is set up
+          // req.session.user = active_user;
+
+          if (remember_me)
+               req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; 
+
+          return res.send(200).json( active_user )
+     } catch(error) {
+          return res.send(200).json({ errorMsg: "An error occured. Please refresh the page and try again." })
+     }
 }
 
 /**
