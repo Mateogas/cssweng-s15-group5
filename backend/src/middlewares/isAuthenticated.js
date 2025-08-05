f277ac17a77cd1b6501c49d35f076207e2d62df5
Mateogas <dckarlmat@gmail.com>
2025-08-05 12:24:38 +0800

    Update isAuthenticated.js

--- backend/src/middlewares/isAuthenticated.js
+++ backend/src/middlewares/isAuthenticated.js
@@ -2,6 +2,9 @@ const isAuthenticated = (req, res, next) => {
     if (req.session.user) {
         next();
     } else {
+        if (req.session.user && !req.session.user.is_active) {
+        req.session.destroy();
+        }
         console.log("authenitcation happened not authenticated: ",req.session.user);
         res.status(401).send('<h1>Not Authorized</h1><p>Please login to continue.</p><a href="/login">Go to Login</a>');// change href to actual deployed page
     }
